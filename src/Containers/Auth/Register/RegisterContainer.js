be9fb56a9f190a50178def71c4872c80fdb4e680
johnclaude02 <johnclaudemaghanoy@gmail.com>
2023-02-16 21:52:03 +0800

    testing navigation

---
+++ src/Containers/Auth/Register/RegisterContainer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { useTranslation } from 'react-i18next'
+import { useTheme } from '@/Hooks'
+import { Brand } from '@/Components'
+
+const RegisterContainer = () => {
+  const { Layout, Fonts } = useTheme()
+
+  const { t } = useTranslation()
+
+  return (
+    <View style={[Layout.fill, Layout.colCenter]}>
+      <Brand />
+      <Text style={Fonts.textCenter}>{t('Gwapo 69 - Register')}</Text>
+    </View>
+  )
+}
+
+export default RegisterContainer
